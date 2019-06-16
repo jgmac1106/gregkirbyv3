ba35f430d609f276edf6a550fe9c96e11727151b
Greg McVerry <jgregmcverry@gmail.com>
2019-06-16 09:24:07 -0400

    revert back to cookbook
    removed my customization and using the exact templates from Kirby cookbook

--- site/templates/article.php
+++ site/templates/article.php
@@ -2,22 +2,14 @@
 <?php snippet('menu') ?>
 
 <section class="content article">
-  <article class="h-entry">
-    <h1 class="p-name"><?= $page->title()->html() ?></h1>
+  <article>
+    <h1><?= $page->title()->html() ?></h1>
+    <?= $page->intro()->kirbytext() ?>
     <?= $page->text()->kirbytext() ?>
 
     <a href="<?= url('blog') ?>">Back…</a>
+
   </article>
-     <div class="p-author h-card">
-      <?php if($author = $page->author()->toUser()): ?>
-  <h2 class="p-name"><?= $author->name() ?></h2>
-  <?php if($avatar = $author->avatar()): ?>
-    <figure>
-      <img class="u-photo" src="<?= $avatar->url() ?>">
-    </figure>
-  <?php endif ?>
-<?php endif ?>
-   </div>
 </section>
 
 <?php snippet('footer') ?>
--- site/templates/blog.php
+++ site/templates/blog.php
@@ -1,31 +1,21 @@
 <?php snippet('header') ?>
 <?php snippet('menu') ?>
 
-<section class="content blog h-feed">
+<main class="blog" role="main">
 
   <h1><?= $page->title()->html() ?></h1>
   <?= $page->text()->kirbytext() ?>
 
   <?php foreach($page->children()->listed()->flip() as $article): ?>
 
-  <article class="h-entry">
-    <h1 class="p-name"><?= $article->title()->html() ?></h1>
-    <p class="p-summary"><?= $article->text()->excerpt(300) ?></p>
+  <article>
+    <h1><?= $article->title()->html() ?></h1>
+    <?= $article->intro()->kirbytext() ?>
     <a href="<?= $article->url() ?>">Read more…</a>
- <aside class="h-card">
-      <?php if($author = $page->author()->toUser()): ?>
-  <h2 class="p-name"><?= $author->name() ?></h2>
-  <?php if($avatar = $author->avatar()): ?>
-    <figure>
-      <img class="u-photo" src="<?= $avatar->url() ?>">
-    </figure>
-  <?php endif ?>
-<?php endif ?>
-</aside>
   </article>
 
   <?php endforeach ?>
 
-</section>
+</main>
 
 <?php snippet('footer') ?>
